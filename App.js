c79d6a54d3753e3177555dedf95270eed2a3e62e
Piyush Chaturvedi <56574565+piyushhawq@users.noreply.github.com>
2024-02-06 23:26:34 +0530

    first episode

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement(
+"h1",
+{id :"heading",xyz:"abc"},
+"hello World from React"
+);
+
+const parent  = React.createElement("div",{id:"parent"},
+[React.createElement("h1",{},"i am an h1 tag"),React.createElement("h2",{},"i am an h2 tag")]
+
+
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
